138d1dfb2807fa5f0a04dc796137e9e49655781d
Maxim <maxim_arslanov@mail.ru>
2021-03-29 08:55:40 +0300

    Deliberate mistake

--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -13,6 +13,7 @@ namespace Infrastructure
     {
         public static void Main(string[] args)
         {
+            SYNTAX ERROR
             CreateHostBuilder(args).Build().Run();
         }
 
